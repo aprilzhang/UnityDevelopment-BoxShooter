55a39408cfc2a8ed00764523efcd0b38c9854918
aprilzhang <aprilsulu@gmail.com>
2016-08-10 23:25:13 +0100

    First two levels.

---
+++ Assets/Scripts/TargetMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMover : MonoBehaviour {
+
+	// define the possible states through an enumeration
+	public enum motionDirections {Spin, RotateAround, Horizontal, Vertical};
+	
+	// store the state
+	public motionDirections motionState = motionDirections.Horizontal;
+
+    // motion parameters
+    public float spinSpeed = 180.0f;
+    public float rotateAroundDistance = 0.0f;
+    public float motionMagnitude = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+
+		// do the appropriate motion based on the motionState
+		switch(motionState) {
+			case motionDirections.Spin:
+				// rotate around the up axix of the gameObject
+				gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+				break;
+            case motionDirections.RotateAround:
+                // rotate around the up axix of the gameObject with a distance from (x,z) = (0,0)
+                gameObject.transform.RotateAround(new Vector3(0, gameObject.transform.position.y, 0), Vector3.up, spinSpeed * Time.deltaTime);
+                break;
+            case motionDirections.Horizontal:
+				// move up and down over time
+				gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+				break;
+			case motionDirections.Vertical:
+				// move up and down over time
+				gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+				break;
+		}
+	}
+}
---
+++ WebGL/README.txt
@@ -0,0 +1,10 @@
+Thanks for reviewing my work.
+
+1. Note that you will need to use Firefox and Safari to load the game from index.html.
+   (ref:Week 1 -Testing WebGL locally in a Browser)
+2. When you start to play the game, there might be a popup saying "Would you like to allow the pointer to be hidden on this site?". 
+   You will need to select "Hide pointer" in order to play the game smoothly.
+3. Level2 has 20 seconds to play and it does not have a level beat score (so you can play util the time runs out). The change i made 
+   in level2 is added Target-Bonus2 and Target-Negative2. I added one more target moving type in TargetMover script called "rotate around", 
+   which is similiar to the rotate around method in our week2 project. The two new targets are using this new moving type.
+4. The gameManager script has been changed in order to have the restart game function in level2.
